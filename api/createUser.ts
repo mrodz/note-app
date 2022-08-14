b1a3e63ca8250b587bc7ef7f7119f289ea9d6c3e
mrodz <79176075+mrodz@users.noreply.github.com>
2022-08-13 23:16:42 -0700

    first commit

---
+++ api/createUser.ts
@@ -0,0 +1,62 @@
+import { prisma, ExpressAppType, logger, buildError } from '.'
+import * as bcrypt from 'bcrypt'
+
+
+async function isUsernameAvailable(username: string) {
+	return await prisma.user.count({
+		where: {
+			username: username
+		}
+	}) === 0
+}
+
+function isValidUsernameChars(username: string) {
+	return /[a-zA-Z0-9]/.test(username)
+}
+
+export function registerUser(app: ExpressAppType) {
+	app.post('/api/register', async (req, res) => {
+		const body = req.body;
+
+		if (!isValidUsernameChars(body.username)) {
+			res.status(400).json(buildError('Invalid Characters', 'A username can only contain letters and numbers'))
+			return
+		}
+
+		let len = body.username.length
+		if (len > 16) {
+			res.status(400).json(buildError('Too Long', `A username can be a maximum of 16 characters long. Yours is ${len}`))
+			return
+		}
+
+		if (!await isUsernameAvailable(body.username)) {
+			res.status(400).json(buildError('Username Taken', `The username '${body.username}' already exists`))
+			return
+		}
+
+		const salt = process.env.P_SALT as string;
+
+		const password = await bcrypt.hash(body.password, salt);
+
+		const newUser = await prisma.user.create({
+			data: {
+				username: body.username,
+				password: password
+			}
+		})
+
+		res.send(newUser)
+	})
+}
+/*
+async function main() {
+	let user = await prisma.user.create({
+		data: {
+			username: '@mrodz',
+			password: '1234'
+		}
+	})
+
+	console.log(user);
+}
+*/
